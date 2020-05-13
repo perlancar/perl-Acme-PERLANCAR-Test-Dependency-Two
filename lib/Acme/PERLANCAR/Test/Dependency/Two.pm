0e6f7b9597ed8c368228a0f74796c59805a22596
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-05-13 08:59:30 +0700

    Initial commit

---
+++ lib/Acme/PERLANCAR/Test/Dependency/Two.pm
@@ -0,0 +1,9 @@
+package Acme::PERLANCAR::Test::Dependency::Two;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Test dependencies
---
+++ lib/Acme/PERLANCAR/Test/Dependency/Two/B.pm
@@ -0,0 +1,9 @@
+package Acme::PERLANCAR::Test::Dependency::Two::B;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Test dependencies
